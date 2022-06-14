982c4fe97ace7d253ff50cec53c4681563e5f4dc
hakamfaza <hakamfaza@gmail.com>
2022-06-14 23:18:28 +0700

    update condition config

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,29 @@
 require('dotenv').config()
 const { Pool } = require('pg')
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } = require('../helpers/env')
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT, NODE_ENV } = require('../helpers/env')
 
-const db = new Pool({
+const config = {
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_DATABASE,
   port: DB_PORT
+}
+
+if (NODE_ENV === 'production') {
+  config.ssl = {
+    rejectUnauthorized: false
+  }
+}
+
+const db = new Pool(config)
+
+db.connect((err) => {
+  if (err) {
+    console.log(err.message)
+    process.exit(1)
+  }
+  console.log('Database berhasil terhubung.')
 })
 
 module.exports = db
